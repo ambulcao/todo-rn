29de51dbdb0a51bbb7d205f850cb7a47e70ec8e9
alexandrebulcao-aluclass <alexandre.bulcao@aluclass.pt>
2023-07-21 11:34:35 +0100

    Animated task label
    Animated task label

--- src/components/task-item.tsx
+++ src/components/task-item.tsx
@@ -1,7 +1,8 @@
 import React, { useCallback } from 'react'
 import { Pressable } from 'react-native'
-import { Box, useTheme, themeTools, useColorModeValue } from 'native-base'
+import { Box, HStack, Text, useTheme, themeTools, useColorModeValue } from 'native-base'
 import AnimatedCheckbox from './animated-checkbox'
+import AnimatedTaskLabel from './animated-task-label'
 
 interface Props {
   isDone: boolean
@@ -33,16 +34,30 @@ const TaskItem = (props: Props) => {
   )
 
   return (
-    <Box width={30} height={30} mr={2}>
-      <Pressable onPress={onToggleCheckbox}>
-        <AnimatedCheckbox 
-          highlightColor={highlightColor}
-          checkmarkColor={checkmarkColor}
-          boxOutlineColor={boxStroke}
-          checked={isDone}
-        />
-      </Pressable>
-    </Box>
+    <HStack 
+      alignItems="center" 
+      w="full" 
+      px={4} 
+      py={2} 
+      bg={useColorModeValue('warmGray.50', 'primary.900')}>
+        <Box width={30} height={30} mr={2}>
+          <Pressable onPress={onToggleCheckbox}>
+            <AnimatedCheckbox 
+              highlightColor={highlightColor}
+              checkmarkColor={checkmarkColor}
+              boxOutlineColor={boxStroke}
+              checked={isDone}
+            />
+          </Pressable>
+        </Box>
+        <AnimatedTaskLabel 
+          textColor={activeTextColor}
+          inactiveTextColor={doneTextColor}
+          strikethrough={isDone}
+        >
+          Task Item
+        </AnimatedTaskLabel>
+    </HStack>
   )
 }
 
