f76a690a7689452847e4e989a959d98d3efde861
alexandrebulcao-aluclass <alexandre.bulcao@aluclass.pt>
2023-08-07 14:28:33 +0100

    Integrate react-navigation
    Integrate react-navigation

---
+++ src/screens/about-screen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Box, Text, VStack } from 'native-base'
+
+const AboutScreen = () => {
+  <VStack flex={1}>
+    <Box>
+      <Text>About</Text>
+    </Box>
+  </VStack>
+}
+
+export default AboutScreen
