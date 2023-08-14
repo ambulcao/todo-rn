5277ddfd8ab0f88d436f896cb7c233274003b196
alexandrebulcao-aluclass <alexandre.bulcao@aluclass.pt>
2023-08-14 14:23:38 +0100

    Animate task label
    Animate task label

--- src/components/task-item.tsx
+++ src/components/task-item.tsx
@@ -1,23 +1,47 @@
 import React, { useCallback } from 'react'
 import { PanGestureHandlerProps } from 'react-native-gesture-handler'
-import { Pressable } from 'react-native'
-import { Box, HStack, Icon, useTheme, themeTools, useColorModeValue } from 'native-base'
+import {
+  NativeSyntheticEvent,
+  Pressable,
+  TextInputChangeEventData
+} from 'react-native'
+import {
+  Box,
+  HStack,
+  Icon,
+  useTheme,
+  themeTools,
+  useColorModeValue,
+  Input
+} from 'native-base'
 import AnimatedCheckbox from './animated-checkbox'
 import AnimatedTaskLabel from './animated-task-label'
 import SwipableView from './swipable-view'
 import { Feather } from '@expo/vector-icons'
 
-
 interface Props extends Pick<PanGestureHandlerProps, 'simultaneousHandlers'> {
+  isEditing: boolean
   isDone: boolean
   onToggleCheckbox?: () => void
   onPressLabel?: () => void
   onRemove?: () => void
+  onChangeSubject?: (subject: string) => void
+  onFinishEditing?: () => void
   subject: string
 }
 
 const TaskItem = (props: Props) => {
-  const { isDone, onToggleCheckbox, subject, onPressLabel, onRemove, simultaneousHandlers } = props
+  const {
+    isEditing,
+    isDone,
+    onToggleCheckbox,
+    subject,
+    onPressLabel,
+    onRemove,
+    onChangeSubject,
+    onFinishEditing,
+    simultaneousHandlers
+  } = props
   const theme = useTheme()
   const highlightColor = themeTools.getColor(
     theme,
@@ -40,22 +64,40 @@ const TaskItem = (props: Props) => {
     useColorModeValue('muted.400', 'muted.600')
   )
 
+  const handleChangeSubject = useCallback(
+    (e: NativeSyntheticEvent<TextInputChangeEventData>) => {
+      onChangeSubject && onChangeSubject(e.nativeEvent.text)
+    },
+    [onChangeSubject]
+  )
+
   return (
-    <SwipableView simultaneousHandlers={simultaneousHandlers}
+    <SwipableView
+      simultaneousHandlers={simultaneousHandlers}
       onSwipeLeft={onRemove}
       backView={
-        <Box w="full" h="full" bg="red.500" alignItems="flex-end" justifyContent="center" pr={4}>
-          <Icon color="white" as={<Feather name="trash-2" />} size="sm" /></Box>
-      }>
-        <HStack 
-      alignItems="center" 
-      w="full" 
-      px={4} 
-      py={2} 
-      bg={useColorModeValue('warmGray.50', 'primary.900')}>
+        <Box
+          w="full"
+          h="full"
+          bg="red.500"
+          alignItems="flex-end"
+          justifyContent="center"
+          pr={4}
+        >
+          <Icon color="white" as={<Feather name="trash-2" />} size="sm" />
+        </Box>
+      }
+    >
+      <HStack
+        alignItems="center"
+        w="full"
+        px={4}
+        py={2}
+        bg={useColorModeValue('warmGray.50', 'primary.900')}
+      >
         <Box width={30} height={30} mr={2}>
           <Pressable onPress={onToggleCheckbox}>
-            <AnimatedCheckbox 
+            <AnimatedCheckbox
               highlightColor={highlightColor}
               checkmarkColor={checkmarkColor}
               boxOutlineColor={boxStroke}
@@ -63,16 +105,31 @@ const TaskItem = (props: Props) => {
             />
           </Pressable>
         </Box>
-        <AnimatedTaskLabel 
-          textColor={activeTextColor}
-          inactiveTextColor={doneTextColor}
-          strikethrough={isDone}
-        >
-          {subject}
-        </AnimatedTaskLabel>
-    </HStack>
-      </SwipableView>
-    
+        {isEditing ? (
+          <Input 
+            placeholder="Task" 
+            value={subject} 
+            variant="unstyled" 
+            fontSize={19}
+            px={1}
+            py={0}
+            autoFocus
+            blurOnSubmit
+            onChange={handleChangeSubject}
+            onBlur={onFinishEditing}
+          />
+        ) : (
+          <AnimatedTaskLabel
+            textColor={activeTextColor}
+            inactiveTextColor={doneTextColor}
+            strikethrough={isDone}
+            onPress={onPressLabel}
+          >
+            {subject}
+          </AnimatedTaskLabel>
+        )}
+      </HStack>
+    </SwipableView>
   )
 }
 
--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -7,12 +7,27 @@ import {
   VStack,
   useColorModeValue
 } from 'native-base'
+import { AntDesign } from '@expo/vector-icons'
 import ThemeToggle from '../components/theme-toggle'
 import TaskItem from '../components/task-item'
+import shortid from 'shortid'
+
+const initialData = [
+  {
+    id: shortid.generate(),
+    subject: 'Buy movie tickets for Friday',
+    done: false
+  },
+  {
+    id: shortid.generate(),
+    subject: 'Make a React Native tutorial',
+    done: false
+  }
+]
 
 export default function MainScreen() {
-  
-  const [ checked, setChecked ] = useState(false)
+  const [data, setData] = useState(initialData)
+  const [editingItemId, setEditingItemId] = useState(null)
   const handlePressCheckbox = useCallback(() => {
       setChecked(prev => !prev)
   }, [])
@@ -20,7 +35,15 @@ export default function MainScreen() {
   return(
     <Center _dark={{bg: 'blueGray.900'}} _light={{bg: 'blueGray.50'}} flex={1}>
       <VStack space={5} alignItems="center" w="full">
-            <TaskItem isDone={checked} onToggleCheckbox={handlePressCheckbox} subject="Task Item"/>
+            <TaskItem 
+              isEditing={isEditing}
+              isDone={checked} 
+              onToggleCheckbox={handlePressCheckbox} 
+              subject={subject}
+              onPressLabel={() => setEditing(true)}
+              onChangeSubject={setSubject}
+              onFinishEditing={() => setEditing(false)}
+            />
         
         <ThemeToggle/>
       </VStack>
